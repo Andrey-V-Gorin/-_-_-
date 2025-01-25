d13341253de86d6a7584221d4b7f143c4ddf2f8f
Andrey-V-Gorin <avg-c2009@yandex.ru>
2025-01-25 09:57:46 +0300

    Update urls.py

--- Python/FinanceManager/main/urls.py
+++ Python/FinanceManager/main/urls.py
@@ -1,1 +1,11 @@
+from django.urls import path
+from . import views
 
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('add/', views.add_transaction, name='add_transaction'),
+    path('reports/', views.reports, name='reports'),
+    
+]
