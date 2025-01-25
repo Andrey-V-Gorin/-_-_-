7d8d067179a4b6594d663e67a344e4e0588423d5
Andrey-V-Gorin <avg-c2009@yandex.ru>
2025-01-25 10:09:10 +0300

    Update urls.py

--- Python/FinanceManager/users/urls.py
+++ Python/FinanceManager/users/urls.py
@@ -1,1 +1,13 @@
+#from django.conf import settings
+#from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('registration/', views.register_view, name="registration"),
+    path('login/', views.login_view, name="login"),
+    path('profile/', views.profile, name="profile"),
+    path('update/', views.update_profile, name='update_profile'),
+    path('logout/', views.logout_view, name='logout'),
+    
+]
